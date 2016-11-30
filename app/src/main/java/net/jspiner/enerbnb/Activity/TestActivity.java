fd23c36eb47f872dedb5ec89d1dc6695f7d65aa9
JSpiner <jspiner@naver.com>
2016-11-30 17:21:58 +0900

    add test activity

---
+++ app/src/main/java/net/jspiner/enerbnb/Activity/TestActivity.java
@@ -0,0 +1,21 @@
+package net.jspiner.enerbnb.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import net.jspiner.enerbnb.R;
+
+/**
+ * Copyright 2016 JSpiner. All rights reserved.
+ *
+ * @author JSpiner (jspiner@naver.com)
+ * @project EnerBnB
+ * @since 2016. 11. 30.
+ */
+public class TestActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
